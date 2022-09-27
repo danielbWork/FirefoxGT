79e3a117f199afa6fbcdef781b95971f19c96b06
danielbwork <danielbwork12@gmail.com>
2022-09-27 12:45:47 +0300

    Fixed remove code
    TODO:
    - Fix move code
    - Add edit group tab name
    - General code overview and cleanup
    - Add Pop up with group tab features
    - Add custom icon for group tab
    - Create custom notification handler
    - Decide what to do with pinned tabs (likely separated groups and var)
    - Add onDetach code
    - Settings page (includes handling the discard stuff)
    - Add Container code
    - Add Icon selection for group tab
    - Add local copy of storage for better performance

--- backgroundScripts/TabHandlers/RemoveTabHandler.js
+++ backgroundScripts/TabHandlers/RemoveTabHandler.js
@@ -1,19 +1,55 @@
-import { removeTabFromStorage } from "../../Storage/StorageHandler.js";
+import {
+  removeTabFromStorage,
+  storageNotifier,
+} from "../../Storage/StorageHandler.js";
+import { GroupTab } from "../GroupTab.js";
 
 /**
  * Handles setup for group tab removing
  */
 export function setupRemoveHandler() {
-  browser.tabs.onRemoved.addListener(removeGroupTab);
+  browser.tabs.onRemoved.addListener(onRemoveTabFromScreen);
+  storageNotifier.onRemoveTab.addListener(onRemoveTabFromStorage);
 }
 
 /**
- * Removes the group tab from the storage
+ * Removes the group tab or inner tab from the storage
  *
  * @param {number} tabId The id of the tab we want to remove
  * @param {} removeInfo Info regarding deleting the tab
  */
-async function removeGroupTab(tabId, removeInfo) {
+async function onRemoveTabFromScreen(tabId, removeInfo) {
   await removeTabFromStorage(tabId);
   // TODO Handle showing the inner tabs/removing after this
+}
+
+/**
+ * Handles showing inner tabs once group tab is removed
+ *
+ * TODO: in future have in setting page setting for doing this or deleting deleting
+ *
+ * @param {GroupTab} groupTab The group tab that was removed (or had an inner tab removed)
+ * @param {number | undefined} index The index of the inner tab if this has a value function does nothing
+ */
+function onRemoveTabFromStorage(groupTab, index) {
+  // Only cares when it group tab
+  if (index) return;
+
+  if (groupTab.isOpen) {
+    browser.notifications.create({
+      type: "basic",
+      iconUrl: "icons/icon.png",
+      title: `Removed ${groupTab.name}`,
+      message: "All inner tabs are currently available",
+    });
+  } else {
+    browser.tabs.show(groupTab.innerTabs);
+
+    browser.notifications.create({
+      type: "basic",
+      iconUrl: "icons/icon.png",
+      title: `Removed ${groupTab.name}`,
+      message: "Displaying all of its hidden tabs",
+    });
+  }
 }
