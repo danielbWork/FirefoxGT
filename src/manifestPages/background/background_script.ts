6d1fcc61c1774850a4b5121bc03c0b8e3a427fc1
danielbwork <danielbwork12@gmail.com>
2022-10-27 15:19:17 +0300

    Removed popup and options from webpages folder Moved them and background to manifestPages Updated package.json to put the created code in correct location Removed page action
    TODO:
    - Fix dragging group tab inside group tab
    - Apply the actual settings (remove, ui, other)
    - Add session code (includes removing group tab from recants)
    - Update dialogs to new ui i.e content scripts (check about running with popup)
    - Maybe change group tab loading in popup (or at least add loadbars)
    - Update popup (and maybe settings) ui and colors

---
+++ src/manifestPages/background/background_script.ts
@@ -0,0 +1,23 @@
+import { StorageHandler } from "../../utils/Storage/StorageHandler";
+import { ContextMenuHandler } from "../../backgroundScripts/ContextMenuHandler";
+import { CreateTabHandler } from "../../backgroundScripts/TabHandlers/CreateTabHandler";
+import { EditTabHandler } from "../../backgroundScripts/TabHandlers/EditTabHandler";
+import { MoveTabHandler } from "../../backgroundScripts/TabHandlers/MoveTabHandler";
+import { RemoveTabHandler } from "../../backgroundScripts/TabHandlers/RemoveTabHandler";
+import browser from "webextension-polyfill";
+import { OnTabClickHandler } from "../../backgroundScripts/TabHandlers/OnTabClickHandler";
+import { BackgroundMessageHandler } from "../../backgroundScripts/BackgroundMessageHandler";
+
+browser.runtime.onInstalled.addListener(async () => {
+  await StorageHandler.instance.setupDefaultStorage();
+  ContextMenuHandler.instance.setupContextMenuItems();
+});
+
+StorageHandler.instance.loadStorage();
+
+CreateTabHandler.instance.setupCreateHandler();
+OnTabClickHandler.instance.setupOnClickHandler();
+RemoveTabHandler.instance.setupRemoveHandler();
+MoveTabHandler.instance.setupMoveHandler();
+EditTabHandler.instance.setupEditHandler();
+BackgroundMessageHandler.instance.setupMessageHandler();
